551967acd310c8f1ae4f192bb6c02a63be26596f
Gorgito <114603819+Gorgito@users.noreply.github.com>
2022-12-07 16:01:24 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,85 @@
+
+import React from 'react';
+import './App.css';
+
+import Busqueda from './Busqueda/Busqueda';
+import ListaContacto from './ListaContactos';
+import Contacto from './Contacto';
+import AgregarContacto from './AgregaContacto';
+import { DirectorioContext,DirectorioProvider} from './Context/DirectorioProvider';
+
+function App() {
+let modal=false;
+
+  return (
+ <DirectorioProvider>
+  <DirectorioContext.Consumer>
+  {
+  ({
+contactosFiltrados,
+borrarcontactos,
+contadorContactos,
+Modal,
+setModal
+
+
+   })=>(
+  <React.Fragment><center>
+    <h1> Directorio[{contadorContactos}]</h1>
+    <Busqueda  />
+    
+    <ListaContacto>
+    {
+      contactosFiltrados.map((contactos)=>{
+     return(
+      <Contacto
+      nombre={contactos.nombre}
+      telefono={contactos.telefono}
+      correo={contactos.correo}
+      borrarcontactos={()=>{borrarcontactos(contactos.nombre)}}
+      />
+     )
+
+      })
+    }
+
+    </ListaContacto>
+    <button onClick={()=>{setModal(true)}}>Agregar</button>
+   {Modal&&<AgregarContacto/>} 
+   </center>
+
+  </React.Fragment>
+
+    )
+  }
+  </DirectorioContext.Consumer>
+ </DirectorioProvider>
+
+
+
+  );
+}
+
+export default App;
+
+/*
+let contactos=[
+  {
+    nombre:"gullermo",
+    telefono:"987654321",
+    correo:"marcelo@gmail.com"
+  }
+,
+  {
+      nombre:"Marcelo",
+      telefono:"987654321",
+      correo:"marcelo@gmail.com"
+  }
+,
+  {
+    nombre:"fabian",
+    telefono:"987654321",
+    correo:"marcelo@gmail.com"
+  }
+]
+*/
